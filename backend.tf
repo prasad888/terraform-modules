5bdb3884931a2a0ca38ebb8842cb52ca46c1426b
pmaddiralla <pmaddiralla@apple.com>
2026-03-20 14:48:00 +0530

    backend updated

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "prasad-s3-demo-xyz"
     key    = "prasad/terraform.tfstate"
-    region = var.region
+    region = ap-south-1
     encrypt = true 
     dynamodb_table = "terraform-lock"   
     
